babb995fab7a121b765afeb32b80bc63b627325f
Muhammad Al Fahad <muhammad.alfahad@nearpeer.org>
2023-06-11 13:31:54 +0500

    changes fixed

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
-require('dotenv').config({
-    path: `.env.development`, // Replace with your desired .env file
-});
-
 module.exports = {
     env: {
         "BASE_URL": "http://127.0.0.1:5000/api",
